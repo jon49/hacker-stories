20cd16213c03593e468c249124576444b052d5e4
Jon Nyman <nymanjon@gmail.com>
2025-01-29 09:22:55 -0700

    Finished through React JSX.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{welcome.greeting}, {welcome.title}!</h1>
+
+      <ul>
+        {list.map(x => <li><a href={x.url}>{x.title}</a></li>)}
+      </ul>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
   )
 }
 
