3bca217af95a28c83e7c241a7e74950dd7786e8e
Jon Nyman <nymanjon@gmail.com>
2025-01-30 08:53:13 -0700

    Lists

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,20 @@ const list = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   },
   {
     title: 'Redux',
     url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
-]
+];
 
 function App() {
   return (
@@ -20,7 +28,7 @@ function App() {
       <h1>{welcome.greeting}, {welcome.title}!</h1>
 
       <ul>
-        {list.map(x => <li><a href={x.url}>{x.title}</a></li>)}
+        {list.map(x => <li key={x.objectID}><a href={x.url}>{x.title}</a></li>)}
       </ul>
 
       <label htmlFor="search">Search: </label>
