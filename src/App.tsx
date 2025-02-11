d663b26ec0edfbe2d176880a0c252f3a0ab558ab
Jon Nyman <nymanjon@gmail.com>
2025-02-11 14:57:58 -0700

    Lifting state

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,41 @@ const welcome = {
   title: 'React',
 }
 
-const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const allStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-  const handleSearch = (searched: string) => {
-    console.log(searched)
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let searched = event.target.value
+    setSearchTerm(searched)
   }
+
+  const stories = allStories.filter(x => x.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <div>
       <h1>{welcome.greeting}, {welcome.title}!</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
 
       <hr />
 
@@ -62,13 +68,10 @@ const StoryItem = ({ story }: { story: Story }) => {
     </li>)
 }
 
-const Search = ({onSearch}: {onSearch: (searched: string) => any}) => {
-  const [searchTerm, setSearchTerm] = React.useState('')
+const Search = ({onSearch, searchTerm}: {onSearch: (event: React.ChangeEvent<HTMLInputElement>) => any, searchTerm: string}) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let term = event.target.value
-    setSearchTerm(term)
-    onSearch(term)
+    onSearch(event)
   }
 
   return (
